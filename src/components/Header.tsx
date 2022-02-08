a9d91f21d0ad3760229c0abd899530bf9325db25
Brent Timmermans <brenttimmermans7@gmail.com>
2022-02-08 22:35:06 +0100

    feat(header): tweak welcome

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,6 +7,7 @@ import Socials from './Common/Socials'
 import { InstagramLink } from '../links'
 
 import ProfilePicture from '../assets/images/profile.jpeg'
+import WaveEmoji from '../assets/images/wave.png'
 
 export default function Header() {
   return (
@@ -19,24 +20,41 @@ export default function Header() {
           margin-bottom: 70px;
         `}
       />
-      <div
-        css={css`
-          > * + * {
-            margin-top: var(--spacing-L);
-          }
-        `}
-      >
-        <H1>Brent Timmermans</H1>
+      <TextWrapper>
+        <div
+          css={css`
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-wrap: wrap;
+          `}
+        >
+          <H1>Hi! I'm Brent&nbsp;Timmermans</H1>
+          <CircularImage
+            src={WaveEmoji}
+            alt="Waving hand emoji"
+            size={40}
+            css={css`
+              margin-left: var(--spacing-S);
+            `}
+          />
+        </div>
         <p>
           Developer · <TextLink href={InstagramLink}>Photographer</TextLink> ·
           Runner
         </p>
         <Socials />
-      </div>
+      </TextWrapper>
     </Wrapper>
   )
 }
 
 const Wrapper = styled.article`
   text-align: center;
+`
+
+const TextWrapper = styled.div`
+  > * + * {
+    margin-top: var(--spacing-L);
+  }
 `
