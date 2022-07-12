2ccf6790b30f1a0375c3421f6985ec66aedaeaf1
Brent Timmermans <brenttimmermans7@gmail.com>
2022-07-12 19:39:50 +0200

    ✨ Update LinkedIn link

--- src/util/links.ts
+++ src/util/links.ts
@@ -1,5 +1,5 @@
 export const InstagramLink = 'https://www.instagram.com/brentt_/'
 export const GithubLink = 'https://github.com/Brent-T'
 export const LinkedinLink =
-  'https://www.linkedin.com/in/brent-timmermans-96846512b/'
+  'https://www.linkedin.com/in/brenttimmermans/'
 export const TwitterLink = 'https://twitter.com/brenttimmermans'
