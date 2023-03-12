9f18d08b38b650bd24feda69aa7bc239b32015d1
Brent Timmermans <brenttimmermans7@gmail.com>
2023-03-12 18:36:59 +0100

    🎨 fix import order for css files

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom'
 import { App } from '~/src/App'
 
 import '~/src/styles/fonts.css'
-import '~/src/styles/main.css'
 import '~/src/styles/normalize.css'
+
+import '~/src/styles/main.css'
 
 const app = document.getElementById('app')
 
